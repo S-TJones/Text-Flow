d1f05579f32bbceb6db15cd31e7935f05e11c4d6
Shemar Jones <47797339+S-TJones@users.noreply.github.com>
2020-10-19 11:06:07 -0500

    Updated Android App
    Imported the FlowText.java file. Made several edits to make it usable in the MainActivity. Customized the MainActivity layout as well to accept user input: both sentence and character limit. It should do all conversion where neccessary and run the TextFlow class methods.

--- Android App/app/src/main/java/com/example/flowtextapplication/MainActivity.java
+++ Android App/app/src/main/java/com/example/flowtextapplication/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.flowtextapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText InputSentence = (EditText) findViewById(R.id.InputSentenceEditText);
+        final EditText InputWidth = (EditText) findViewById(R.id.InputWidthEditText);
+
+        Button FindSolution = (Button) findViewById(R.id.FindButton);
+
+        final TextView ResultText = (TextView) findViewById(R.id.ResultTextView);
+        final TextView ResultRiver = (TextView) findViewById(R.id.RiverTextView);
+
+        FindSolution.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Convert the user inputs into correct data types.
+                String Sentence = InputSentence.toString();
+                String stringWidth = String.valueOf(InputWidth);
+                int Width;
+
+                // Checks to see if a width was entered by the user.
+                if (stringWidth.equals("")){
+                    Width = 0;
+                } else {
+                    Width = Integer.parseInt(stringWidth);
+                }
+
+                //Calls the findFlow method.
+                // It calculates the
+                String[] lines = findFlow(Sentence, Width);
+
+                // Adds visibility and text to the TextViews
+                ResultText.setVisibility(View.VISIBLE);
+                ResultText.setText(lines[0]);
+                ResultRiver.setVisibility(View.VISIBLE);
+                ResultRiver.setText(lines[1]);
+            }
+        });
+    }
+
+    private String[] findFlow(String sentence, int width){
+
+        String[] output = new String[2];
+        int total = sentence.split(" ").length;
+        ArrayList<String> wrapped;
+        int defaultWidth = 15;
+
+        // If the width is zero, the default width is used.
+        if (width == 0){
+            width = defaultWidth;
+        }
+
+        wrapped = TextFlow.autoWrap(sentence, width);
+
+        String rivers = TextFlow.locateRivers(wrapped);
+        rivers = "Final text && marked maximum flow with red *.\n" + rivers;
+        output[1] = rivers;
+
+        String goals = "Total words: " + total + ", Best width: " + width + ", Max flow: " + TextFlow.getLongest();
+        output[0] = goals;
+
+        return output;
     }
 }
---
+++ Android App/app/src/main/java/com/example/flowtextapplication/TextFlow.java
@@ -0,0 +1,149 @@
+package com.example.flowtextapplication;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public final class TextFlow {
+
+    private static String end;
+    private static int longest; // Represents longest river length
+
+    // Constructor
+    public TextFlow() {
+        end = "$#END";
+    }
+
+    // Setter for longest river length attribute.
+    public static void setLongest(int length){
+        longest = length;
+    }
+
+    // Getter for longest river length attribute.
+    public static int getLongest(){
+        return longest;
+    }
+
+    public static ArrayList<String> autoWrap(String sentence, int limit) {
+        String newSentence = sentence + " " + end;
+        String[] allWords = newSentence.split(" ");
+
+        StringBuilder line = new StringBuilder();
+        int wordLength = line.length();
+        ArrayList<String> sentenceList = new ArrayList<String>();
+
+        for (String word : allWords) {
+            if (word.equals(end)) {
+                sentenceList.add(line.toString());
+                break;
+            }
+
+            wordLength = wordLength + word.length();
+
+            if (wordLength <= limit) {
+                line.append(word).append(" ");
+                wordLength++;
+            } else {
+                sentenceList.add(line.toString());
+                line = new StringBuilder();
+                line.append(word).append(" ");
+                wordLength = line.length();
+            }
+        }
+
+        for (String string : sentenceList) {
+            System.out.println(string);
+        }
+
+        return sentenceList;
+    }
+
+    public static String locateRivers(ArrayList<String> lineList) {
+        ArrayList<ArrayList<Integer>> spaceList = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> potentialRiver = new ArrayList<ArrayList<Integer>>();
+        int length = lineList.size();
+
+        for (String line : lineList) {
+
+            String newLine = line.trim();
+            ArrayList<Integer> lst = new ArrayList<Integer>();
+
+            for (int x = 0; x < newLine.length(); x++) {
+                if (newLine.charAt(x) == ' ') {
+                    lst.add(x);
+                }
+            }
+            spaceList.add(lst);
+        }
+
+        for (int a = 0; a < length; a++) {
+            for (Integer index : spaceList.get(a)) {
+                int spot = index;
+                // Keeps track the the line number and index numbers.
+                ArrayList<Integer> lst = new ArrayList<Integer>();
+                lst.add(a);
+                lst.add(spot);
+
+                for (ArrayList<Integer> indexList : spaceList.subList(a + 1, spaceList.size())) {
+                    if (indexList.contains(spot - 1)) {
+                        spot = spot - 1;
+                        lst.add(spot);
+                    } else if (indexList.contains(spot)) {
+                        lst.add(spot);
+                    } else if (indexList.contains(spot + 1)) {
+                        spot = spot + 1;
+                        lst.add(spot);
+                    } else {
+                        break;
+                    }
+                }
+                potentialRiver.add(lst);
+            }
+
+            ArrayList<ArrayList<Integer>> rivers = new ArrayList<ArrayList<Integer>>();
+
+            int riverLength = 0;
+            for (ArrayList<Integer> element : potentialRiver) {
+                int max = element.size();
+                if (max > riverLength) {
+                    riverLength = max;
+                }
+            }
+
+            setLongest(riverLength);
+
+            for (ArrayList<Integer> element : potentialRiver) {
+                int listSize = element.size();
+                if (listSize == getLongest()) {
+                    rivers.add(element);
+                }
+            }
+
+            for (ArrayList<Integer> element : rivers) {
+                for (int num = 0; num < length; num++) {
+                    if (num == element.get(0)) {
+                        for (Integer index : element.subList(1, element.size())) {
+                            String first = lineList.get(num).substring(0, index);
+                            String asterisk = "*";
+                            String second = lineList.get(num).substring(index + 1, lineList.get(num).length());
+                            String newString = first + asterisk + second;
+
+                            lineList.set(num, newString);
+                            num++;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        StringBuilder riverLines = new StringBuilder("");
+        // Adds a println at the end of each line.
+        for (String string : lineList) {
+            String rLine = string + "\n";
+            riverLines.append(rLine);
+        }
+
+        return riverLines.toString();
+    }
+
+}
